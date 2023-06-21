53295b8b1c2614e144adc5e3105f3a9da3e428cb
Mook DD <135083436+Mookth789@users.noreply.github.com>
2023-06-21 21:24:45 +0000

    Update 6 files

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '13913c53a36e953e48d5e757b1d6c5a9',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
